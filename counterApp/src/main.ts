43e454012d59d41a591f74cf7dc51b73a45c4c4a
coderAjju <upadhyayajay437@gmail.com>
2024-03-27 17:57:09 +0530

    typescript project added

--- counterApp/src/main.ts
+++ counterApp/src/main.ts
@@ -1,41 +1,46 @@
 const ope = document.querySelectorAll<HTMLDivElement>(".operator");
 const res = document.querySelector<HTMLDivElement>("#res");
-const changeValue = document.querySelector<HTMLInputElement>("#changeValue")
-if(changeValue){
-  changeValue.value = "1"
+const changeValue = document.querySelector<HTMLInputElement>("#changeValue");
+if (changeValue) {
+  changeValue.value = "1";
 }
 if (res) {
-    res.innerHTML = "0";
+  res.innerHTML = "0";
 }
-const arr: string[] = ['-', '+'];
+const arr: string[] = ["-", "+"];
 
 ope.forEach((element, index) => {
-    element.innerHTML = arr[index];
+  element.innerHTML = arr[index];
 });
 
 ope.forEach((elem) => {
-    elem.addEventListener('click', (e) => {
-        if (e.target.innerHTML === '-') {
-            if (res) {
-                const currentValue = parseInt(res.innerHTML);
-                res.innerHTML = (currentValue - parseInt(changeValue.value)).toString();
-            }
+  if (elem) {
+    elem.addEventListener("click", () => {
+      if (elem.innerHTML === "-") {
+        if (res && changeValue) {
+          const currentValue = parseInt(res.innerHTML);
+          res.innerHTML = (
+            currentValue - parseInt(changeValue.value)
+          ).toString();
         }
-        else if(e.target.innerHTML === '+'){
-          if(res){
-            const currentValue = parseInt(res.innerHTML);
-            res.innerHTML = (currentValue + parseInt(changeValue.value)).toString();
-          }
+      } else if (elem.innerHTML === "+") {
+        if (res && changeValue) {
+          const currentValue = parseInt(res.innerHTML);
+          res.innerHTML = (
+            currentValue + parseInt(changeValue.value)
+          ).toString();
         }
+      }
     });
+  }
 });
 
 const resetButton = document.querySelector<HTMLButtonElement>("#reset");
 if (resetButton) {
-    resetButton.addEventListener("click", () => {
-        const res = document.querySelector<HTMLDivElement>("#res");
-        if (res) {
-            res.innerHTML = "0";
-        }
-    });
+  resetButton.addEventListener("click", () => {
+    const res = document.querySelector<HTMLDivElement>("#res");
+    if (res) {
+      res.innerHTML = "0";
+    }
+  });
 }
